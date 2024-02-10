33ec73610137db029436ba083d7432ffcf91dcce
Povshy <vladcarrib@gmail.com>
2024-02-10 14:31:11 +0200

    222

--- src/route/index.js
+++ src/route/index.js
@@ -4,55 +4,92 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
-class User {
+class Track {
   static #list = []
 
-  constructor(email, login, password) {
-    this.email = email
-    this.login = login
-    this.password = password
-    this.id = new Date().getTime()
+  constructor(name, author, image) {
+    this.id = Math.floor(1000 + Math.random() * 9000)
+    this.name = name
+    this.author = author
+    this.image = image
   }
 
-  verifyPassword = (password) => this.password === password
-
-  static add = (user) => {
-    this.#list.push(user)
+  static create(name, author, image) {
+    const newTrack = new Track(name, author, image)
+    this.#list.push(newTrack)
+    return newTrack
   }
 
-  static getList = () => this.#list
+  static getList() {
+    return this.#list.reverse()
+  }
+}
 
-  static getById = (id) =>
-    this.#list.find((user) => user.id === id)
+Track.create(
+  'Plyama kosmosu',
+  'Valkinos',
+  '/https://picsum.photos/100/100',
+)
 
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id,
-    )
+Track.create(
+  'Okean Plazmu',
+  'Korova kakala',
+  '/https://picsum.photos/100/100',
+)
 
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
+Track.create(
+  'Tartar',
+  'Ponad edgarpid',
+  '/https://picsum.photos/100/100',
+)
+
+Track.create(
+  'Bylo',
+  'Nasho',
+  '/https://picsum.photos/100/100',
+)
+
+Track.create(
+  'Karavan',
+  'Zamok chu zamok',
+  '/https://picsum.photos/100/100',
+)
+
+Track.create(
+  'Pino-coka',
+  'Parad vechirok',
+  '/https://picsum.photos/100/100',
+)
+
+console.log(Track.getList())
+// ================================================================
+class Playlist {
+  static #list = []
+
+  constructor(name) {
+    this.id = Math.floor(1000 + Math.random() * 9000)
+    this.name = name
+    this.track = []
   }
 
-  static updateById = (id, data) => {
-    const user = this.getById(id)
-
-    if (user) {
-      this.update(user, data)
-      return true
-    } else {
-      return false
-    }
+  static create(name) {
+    const newPlaylist = new Playlist(name)
+    this.#list.push(newPlaylist)
+    return newPlaylist
   }
 
-  static update = (user, { email }) => {
-    if (email) {
-      user.email = email
-    }
+  static getList() {
+    return this.#list.reverse()
+  }
+
+  static makeMix(playlist) {
+    const allTracks = Track.getList()
+
+    let randomTracks = allTracks
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 3)
+
+    playlist.tracks.push(...randomTracks)
   }
 }
 // ================================================================
@@ -63,70 +100,61 @@ class User {
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
-  const list = User.getList()
-
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('spotify-choose', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
+    style: 'spotify-choose',
 
-    data: {
-      users: {
-        list,
-        isEmpty: list.length === 0,
-      },
-    },
+    data: {},
   })
   // ↑↑ сюди вводимо JSON дані
 })
+// =======================================
+router.get('/spotify-create', function (req, res) {
+  const isMix = !!req.query.isMix
 
-// ================================================================
+  console.log(isMix)
 
-router.post('/user-create', function (req, res) {
-  const { email, login, password } = req.body
+  res.render('spotify-create', {
+    style: 'spotify-create',
 
-  const user = new User(email, login, password)
-
-  User.add(user)
-
-  console.log(User.getList())
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач створений',
+    data: {
+      isMix,
+    },
   })
 })
-// ================================================================
+// =======================================
+router.post('/spotify-create', function (req, res) {
+  const isMix = !!req.query.isMix
 
-router.get('/user-delete', function (req, res) {
-  const { id } = req.query
+  const name = req.body.name
 
-  User.deleteById(Number(id))
+  if (!name) {
+    return res.render('alert', {
+      style: 'alert',
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач видалений',
-  })
-})
-// ================================================================
-
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body
-
-  let result = false
-
-  const user = User.getById(Number(id))
-
-  if (user.verifyPassword(password)) {
-    User.update(user, { email })
-    result = true
+      data: {
+        message: 'Помилка',
+        info: 'Введіть назву плейліста',
+        link: isMix
+          ? '/spotify-create?isMix=true'
+          : '/spotify-create',
+      },
+    })
   }
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: result
-      ? 'Email пошта оновлена'
-      : 'Сталася помилка',
+  const playlist = Playlist.create(name)
+
+  if (isMix) {
+    Playlist.makeMix(playlist)
+  }
+
+  console.log(playlist)
+
+  res.render('spotify-create', {
+    style: 'spotify-create',
+
+    data: {},
   })
 })
 
